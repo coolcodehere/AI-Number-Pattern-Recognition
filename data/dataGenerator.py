168a652b4481c71aeddcc285802587fba54056a4
noahrr2 <noahrr2@illinois.edu>
2019-09-21 22:05:02 +0000

    restructured

--- data/dataGenerator.py
+++ data/dataGenerator.py
@@ -1,1 +1,16 @@
-dataGenerator.py
+import random
+
+dataset = open("data.txt", "w")
+
+def generateData(numLines)
+    """
+    Arguments:
+    numLines: Number of lines to generate
+
+    Generates numLines strings of 30 integers in data.txt
+    """
+    for i in range(0, numLines):
+        numString = ""
+        for i in range(0, 30):
+            numString += str(random.randint(0, 9))
+        dataset.write(numString + "\n")
